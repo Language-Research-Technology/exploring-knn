291225818027c56fe71e5498650872885d9de07b
Jasper Chong <74451969+jasperchongcode@users.noreply.github.com>
2025-02-10 10:00:16 +1000

    reading from zip file

--- dataset.py
+++ dataset.py
@@ -3,13 +3,14 @@ import os
 from PIL import Image
 from torch.utils.data import Dataset
 import torch
+import zipfile
 # print("Worker here")
 
 
 class ImageDataset(Dataset):
     """Image dataset ."""
 
-    def __init__(self, pillow_images, image_filenames, transform=None):
+    def __init__(self, image_zip, image_extensions, transform=None):
         """
         Arguments:
             pillow_images (list): a list of pillow Image objects (matching image_filenames)
@@ -17,12 +18,16 @@ class ImageDataset(Dataset):
             transform (callable, optional): Optional transform to be applied
                 on a sample.
         """
-        self.pillow_images = pillow_images
-        self.filenames = image_filenames
+        # pillow_images = [Image.open(imgzip.open(image_filename)) for image_filename in name_list]
+        imgzip = zipfile.ZipFile(image_zip)
+
+        self.imgzip = imgzip
+        self.name_list = [name for name in imgzip.namelist(
+        ) if name.endswith(image_extensions)]
         self.transform = transform
 
     def __len__(self):
-        return len(self.pillow_images)
+        return len(self.name_list)
 
     def __getitem__(self, idx):
 
@@ -31,7 +36,7 @@ class ImageDataset(Dataset):
         #! figure out indexing multiple?
 
         # print("TAKING INDEX", idx)
-        img = self.pillow_images[idx]
+        img = Image.open(self.imgzip.open(self.name_list[idx]))
 
         # Load an image
         processed_img = img.convert("RGB")  # Ensure it's in RGB mode
@@ -40,4 +45,4 @@ class ImageDataset(Dataset):
         if self.transform:
             processed_img = self.transform(processed_img)
 
-        return processed_img, self.filenames[idx]
+        return processed_img, self.name_list[idx]
